3fb6ba574df8b7bb08f7adcc779f1dffb8cb6fb5
jennkazda <jenkazda@gmail.com>
2019-03-05 18:04:53 -0500

    Add files via upload

---
+++ pins.h
@@ -0,0 +1,10 @@
+
+const byte STDBYE = 12;
+const byte AIN1 = 4;
+const byte AIN2 = 5;
+const byte PWMA = 11;
+const byte BIN1 = 7;
+const byte BIN2 = 8;
+const byte PWMB = 10;
+const byte interruptPinL = 2;
+const byte interruptPinR = 3;
