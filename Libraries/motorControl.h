59ec4c4d41fa89bb020cf854b9e28e295f76e24d
jennkazda <jenkazda@gmail.com>
2019-03-05 19:44:45 -0500

    Update and rename drive.h to motorControl.h

--- Libraries/motorControl.h
+++ Libraries/motorControl.h
@@ -1,4 +1,13 @@
 #include "Arduino.h"
+
+const byte STDBYE = 12;
+const byte AIN1 = 4;
+const byte AIN2 = 5;
+const byte PWMA = 11;
+const byte BIN1 = 7;
+const byte BIN2 = 8;
+const byte PWMB = 10;
+
 class drive
 {
 public:
