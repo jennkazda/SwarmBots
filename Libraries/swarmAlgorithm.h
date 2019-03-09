05cdfb75b50922dd42cb2bd6e1846b0fc212e695
josuemendez94 <40776830+josuemendez94@users.noreply.github.com>
2019-03-09 00:10:53 -0500

    Update swarmAlgorithm.h

--- Libraries/swarmAlgorithm.h
+++ Libraries/swarmAlgorithm.h
@@ -47,36 +47,4 @@ struct bot {
 	byte direction;
 };
 
-/*  FUNCTION:
- *		initializeBot
- *
- *  DESIGN DESCRIPTION:
- *		Initializes each bot struct to a default value defined in the beginning of the swarmAlgorithm.h
- *
- *  PARAMETER LIST:
- *		bot* botName: bot struct
- *		unsigned int pipeAddr: the pipe address in which the bot will be assigned
- *	
- *  RETURNED:
- *		None.
- *
- *  DESIGN OUTLINE:
- *		1) Initialse struct values to default 
- *
- *  DESIGN CONSTRAINTS:
- *		None.
- *  ========================================================================================*/
-
-void initializeBot(bot* botName, unsigned int pipeAddr) {
-	botName->x = DEFAULT;
-	botName->y = DEFAULT;
-	botName->pipe = pipeAddr;
-	botName->lastMove = DEFAULT;
-	botName->direction = DEFAULT;
-}
-
-
-
-
-
-
+void initializeBot(bot* botName, unsigned int pipeAddr);
