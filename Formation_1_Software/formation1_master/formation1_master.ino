2f64aaff6698fd8f6187078fae2a05dbbdf023af
josuemendez94 <40776830+josuemendez94@users.noreply.github.com>
2019-03-09 21:12:39 -0500

    Update formation1_master.ino

--- Formation_1_Software/formation1_master/formation1_master.ino
+++ Formation_1_Software/formation1_master/formation1_master.ino
@@ -28,6 +28,7 @@
 #define DEBUG_SEND
 #define DEBUG_MOVE
 #define DEBUG_UPDATE
+#define NUM_SLAVES 2
 
 RF24 Radio(CE, CSN);
 Adafruit_VL6180X distSensor = Adafruit_VL6180X(); 
@@ -35,32 +36,71 @@ drive Drive;
 swarm Swarm;
 RF_Sense RF;
 masterState mState;
+slaveState sState;
+bot mBot;
+bot sBot[NUM_SLAVES];
 
-bool result = false;
+
+/*
+#ifdef ONE_SLAVE 
+  bot s1Bot;
+  unsigned int slavePipe [] = {0x01};
+  Swarm.initializeBot(s1Bot, slavePipe[0]);
+#endif
+
+#ifdef TWO_SLAVES 
+  bot s1Bot;
+  bot s2Bot;
+  unsigned int slavePipe [] = {0x01, 0x02};
+  Swarm.initializeBot(s1Bot, slavePipe[0]);
+  Swarm.initializeBot(s2Bot, slavePipe[1]);
+#endif
+
+#ifdef THREE_SLAVES 
+  bot s1Bot;
+  bot s2Bot;
+  bot s3Bot;
+  unsigned int slavePipe [] = {0x01, 0x02, 0x03};
+  Swarm.initializeBot(s1Bot, slavePipe[0]);
+  Swarm.initializeBot(s2Bot, slavePipe[1]);
+  Swarm.initializeBot(s3Bot, slavePipe[2]);
+#endif
+
+#ifdef FOUR_SLAVES 
+  bot s1Bot;
+  bot s2Bot;
+  bot s3Bot;
+  bot s4Bot;
+  unsigned int slavePipe [] = {0x01, 0x02, 0x03, 0x04};
+  Swarm.initializeBot(s1Bot, slavePipe[0]);
+  Swarm.initializeBot(s2Bot, slavePipe[1]);
+  Swarm.initializeBot(s3Bot, slavePipe[2]);
+  Swarm.initializeBot(s4Bot, slavePipe[3]);
+#endif
+
+#define NUM_OF_SLAVES (sizeof(slavePipe)/sizeof(slavePipe[0]))
+*/
+#define STOP    0x00
+#define FORWARD 0x01
+#define REVERSE 0x02
+#define LEFT    0x03
+#define RIGHT   0x04
+
+bool result     = false;
 int state       = mState;
-uint8_t Stop    = 0x00;
-uint8_t Forward = 0x01;
-uint8_t Reverse = 0x02;
-uint8_t Left    = 0x03;
-uint8_t Right   = 0x04; 
 uint8_t command = 0x00;
 unsigned int commandPipe  =  0xFF;
-unsigned int slavePipe [] = {0x01, 0x02, 0x03, 0x04};
 
 void setup(){
     Serial.begin(9600);
-    //printf_begin();
+    // printf_begin();
     Radio.begin();
-    Radio.openReadingPipe(1,commandPipe);
-    Radio.startListening();
+    Radio.openWritingPipe(commandPipe);
+    Radio.stopListening();
     Radio.setPALevel(RF24_PA_MIN);
     //Radio.printDetails();
     Drive.stopMotor();
-
-    // wait for serial port to open on native usb devices
-    while(!Serial){
-        delay(1);
-    }
+    Swarm.initializeBot(sBot, NUM_SLAVES);
 }
 
 void loop(){
@@ -75,7 +115,8 @@ void loop(){
           Serial.println("IDLE");
         #endif
 
-        Radio.read(&command, sizeof(command));
+        /* Reads in command data from command center */
+        Radio.read(&command, sizeof(command));  
         
 //        if(command != Stop){}
 //        else if(command != Forward){}
@@ -91,10 +132,21 @@ void loop(){
         #ifdef DEBUG_SEND
           Serial.println("SEND");
         #endif
-        
-        RF.writingPipe(&Radio, slavePipe[0]);
-        result = Radio.write(&command, sizeof(command));
-        
+
+        /* Master bot transmits command data to slave bot receivers (Slave bots 0-3) */
+        for(int i = 0; i < NUM_SLAVES; i++)
+        {
+          #ifdef DEBUG_SEND
+            Serial.print("Slave Pipe: "); Serial.println(i);
+          #endif
+
+          /* Switch radio pipes to transmit to different bots */
+          RF.writingPipe(&Radio, sBot[i].pipe);
+
+          /* Acknowledges transmitted message was received by distant end */
+          while(Radio.write(&command, sizeof(command)) == false);
+        }
+
         state++;
       break;
 /**********************************************************************************************/
@@ -103,7 +155,33 @@ void loop(){
         #ifdef DEBUG_MOVE
           Serial.println("MOVE");
         #endif
-      
+
+         /* Master moves according to command data */
+         switch(command){
+            case FORWARD :
+              Drive.forward(50);
+              break;
+
+            case REVERSE:
+              Drive.backward(50);
+              break;
+
+            case LEFT:
+              Drive.left90DegreeTurn();
+              break;
+
+            case RIGHT:
+              Drive.right90DegreeTurn();
+              break;
+
+            case STOP:
+              Drive.stopMotor();
+              break;
+
+            default: 
+              Drive.stopMotor();
+              break;
+          }
       state++;
       break;
 /**********************************************************************************************/
@@ -116,8 +194,7 @@ void loop(){
       state++;
       break;
 /**********************************************************************************************/      
-      }
+    }
   }
-  
 }  
  
