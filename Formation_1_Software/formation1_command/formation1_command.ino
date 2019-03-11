be5a4aef3b11f9a140f99de37bf6a57b4a14ea51
jennkazda <jenkazda@gmail.com>
2019-03-11 18:40:24 -0400

    Update formation1_command.ino

--- Formation_1_Software/formation1_command/formation1_command.ino
+++ Formation_1_Software/formation1_command/formation1_command.ino
@@ -24,34 +24,32 @@
 */
 
 #include "swarmAlgorithm.h"
-
-RF24 Radio(CE, CSN);
-unsigned int commandPipe  =  0xFF;
+#include "commandCenter.h"
+RF24 Radio(9,10);
+unsigned int commandPipe  =  0xff;
 
 RF_Sense RF;                                            //initalizes RF class
 
-uint8_t Stop = 0x00;                                    //value to stop motors
-uint8_t Forward = 0x01;                                 //value to move forward
-uint8_t Reverse = 0x02;                                 //value to move backwards
-uint8_t Left = 0x03;                                    //value to pivot left
-uint8_t Right = 0x04;                                   //value to pivot right
 uint8_t command = 0x00;                                 //default value for command to be sent from master
+bool isReady;
 
-int const UP_BTN = 2;
-int const DOWN_BTN = 4;
-int const LEFT_BTN = 5;
-int const RIGHT_BTN = 3;
-int const E_BTN = 6;
-int const F_BTN = 7;
-int const JOYSTICK_BTN = 8;
-int const JOYSTICK_AXIS_X = A0;
-int const JOYSTICK_AXIS_Y = A1;
-int buttons[] = {UP_BTN, DOWN_BTN, LEFT_BTN, RIGHT_BTN, E_BTN, F_BTN, JOYSTICK_BTN};
+unsigned int  STOP = 0x00;                                      //value to stop motors
+unsigned int  FORWARD = 0x01;                                   //value to move forward
+unsigned int  REVERSE = 0x02;                                   //value to move backwards
+unsigned int  LEFT = 0x03 ;                                     //value to pivot left
+unsigned int  RIGHT = 0x04;                                     //value to pivot right
 
+enum commandState{
+  C_IDLING = 0,
+  C_GET,
+  C_SEND
+};
+
+commandState cState;
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  for (int i; i < 7; i++)  {
+  
+  pinMode(LED_BUILTIN, OUTPUT);                       //initialize digital pin LED_BUILTIN as an output.
+  for (int i; i < 7; i++)  {                          //initialize all buttons on the joystick to be input buttons
     pinMode(buttons[i], INPUT_PULLUP);
   }
   Serial.begin(9600);               
@@ -59,10 +57,10 @@ void setup() {
   Radio.openReadingPipe(1, commandPipe);
   Radio.startListening();
   Radio.setPALevel(RF24_PA_MIN);
-  //printf_begin(); 
-  //Radio.printDetails();
+  printf_begin(); 
+  Radio.printDetails();
  
-  while(!Serial){                                     // wait for serial port to open on native usb devices
+  while(!Serial){                                      // wait for serial port to open on native usb devices
       delay(1);
   }
   
@@ -70,56 +68,82 @@ void setup() {
 
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);                     //turn the LED off by making the voltage LOW
   
+  isReady = 0;
+  //Serial.println(commandPipe);
   RF.readPipe(&Radio, 1, commandPipe);
-  RF.writingPipe(&Radio, commandPipe);
-  
-  bool isReady;
-  isReady = false;
-  Radio.read(&isReady, sizeof(isReady));
-  if (isReady == true){
-    digitalWrite(LED_BUILTIN, HIGH);
-    if (!digitalRead(UP_BTN)){
-      command = Stop;
-    }
-    else if (!digitalRead(DOWN_BTN)){
-      command = Stop;
-    }
-    else if (!digitalRead(LEFT_BTN)){
-      command = Left;
-    }
-    else if (!digitalRead(RIGHT_BTN)){
-      command = Right;
-    }
-    else if (!digitalRead(E_BTN)){
-      command = Stop;
-    }
-    else if (!digitalRead(F_BTN)){
-      command = Stop;
-    }
-    else if (!digitalRead(JOYSTICK_BTN)){
-      command = Stop;
-    }
-    else if (map(analogRead(JOYSTICK_AXIS_X), 0, 668, -1, 1) == 0 && map(analogRead(JOYSTICK_AXIS_Y), 0, 668, -1, 1) == 1){
-      command = Forward;
-    }
-    else if (map(analogRead(JOYSTICK_AXIS_X), 0, 668, -1, 1) == -1 && map(analogRead(JOYSTICK_AXIS_Y), 0, 668, -1, 1) == 0){
-      command = Left;
-    }
-    else if (map(analogRead(JOYSTICK_AXIS_X), 0, 668, -1, 1) == 1 && map(analogRead(JOYSTICK_AXIS_Y), 0, 668, -1, 1) == 0){
-      command = Right;
-    }
-    else if (map(analogRead(JOYSTICK_AXIS_X), 0, 668, -1, 1) == 0 && map(analogRead(JOYSTICK_AXIS_Y), 0, 668, -1, 1) == -1){
-      command = Reverse;
-    }
-    else {
-      command = Stop;
-    }
+  while(Radio.available()){
 
-    Radio.write(&command, sizeof(command));
+    switch(cState){
+      case C_IDLING:
+                            //opens rf pipe      
+            Serial.print("isntReady: ");
+            Serial.println(isReady);
+            Radio.read(&isReady, sizeof(isReady));
+            if (isReady == 1){
+              digitalWrite(LED_BUILTIN, HIGH);
+              Serial.print("isReady: ");
+              Serial.println(isReady);
+              cState = C_GET;
+              isReady = 0;
+            }
+            else{
+              cState = C_IDLING;
+            }
+            break;
 
+      case C_GET:
+            if (!digitalRead(UP_BTN)){
+              command = STOP;
+            }
+            else if (!digitalRead(DOWN_BTN)){
+              command = STOP;
+            }
+            else if (!digitalRead(LEFT_BTN)){
+              command = LEFT;
+            }
+            else if (!digitalRead(RIGHT_BTN)){
+              command = RIGHT;
+            }
+            else if (!digitalRead(E_BTN)){
+              command = STOP;
+            }
+            else if (!digitalRead(F_BTN)){
+              command = STOP;
+            }
+            else if (!digitalRead(JOYSTICK_BTN)){
+              command = STOP;
+            }
+            else if (map(analogRead(JOYSTICK_AXIS_X), 0, 668, -1, 1) == 0 && map(analogRead(JOYSTICK_AXIS_Y), 0, 668, -1, 1) == 1){
+              command = FORWARD;
+            }
+            else if (map(analogRead(JOYSTICK_AXIS_X), 0, 668, -1, 1) == -1 && map(analogRead(JOYSTICK_AXIS_Y), 0, 668, -1, 1) == 0){
+              command = LEFT;
+            }
+            else if (map(analogRead(JOYSTICK_AXIS_X), 0, 668, -1, 1) == 1 && map(analogRead(JOYSTICK_AXIS_Y), 0, 668, -1, 1) == 0){
+              command = RIGHT;
+            }
+            else if (map(analogRead(JOYSTICK_AXIS_X), 0, 668, -1, 1) == 0 && map(analogRead(JOYSTICK_AXIS_Y), 0, 668, -1, 1) == -1){
+              command = REVERSE;
+            }
+            else {
+              command = STOP;
+            }
+            Serial.print("Command = ");
+            Serial.println(command);
+            cState = C_SEND;
+            break;
+            
+    case C_SEND:
+            Serial.print("Sending command: ");
+            Serial.println("command");
+            RF.writingPipe(&Radio, commandPipe);
+            Radio.write(&command, sizeof(command));
+            Serial.println("command sent ");
+            Serial.println(" ");
+            cState = C_IDLING;
+            break;
+   }
   }
-
-  
 }
